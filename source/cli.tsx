24fdf27ca6d9b633461c1aa7f0edd6f7c6bf0930
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-05 12:28:02 +0200

    init

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ website-screenshot
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ website-screenshot --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
