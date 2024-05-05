ef6c6d7dd22c765ed0d504f1a4acb1db79f345ba
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-05 12:58:06 +0200

    update the flags

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,29 +1,64 @@
 #!/usr/bin/env node
-import React from 'react';
-import {render} from 'ink';
-import meow from 'meow';
-import App from './app.js';
+import React from 'react'
+import { render } from 'ink'
+import meow from 'meow'
+import App from './app.js'
+
+function parseJson(optionValue: string) {
+  try {
+    return JSON.parse(optionValue)
+  } catch (error) {
+    console.error(`Error parsing JSON input for ${optionValue}:`, error)
+    return { width: 1200, height: 630 } // Default value in case of error
+  }
+}
 
 const cli = meow(
-	`
-	Usage
-	  $ website-screenshot
+  `
+  Usage
+    $ your-command [input]
 
-	Options
-		--name  Your name
+  Options
+    --url, -u <type> Input URL (required)
+    --transform, -t Transform origin [Default: false]
+    --timeout, -T <type> Timeout in milliseconds [Default: 180000]
+    --extension, -e <type> File extension [Default: webp]
+    --concurrency, -c <type> Concurrency level [Default: 3]
+    --quality, -q <type> Image quality [Default: 100]
+    --max-screenshots, -m <type> Maximum number of screenshots [Default: 0]
+    --recommended-size, -r <type> Recommended image size [Default: {"width":1200,"height":630}]
+    --output-dir, -o <type> Output directory [Default: ./public/screenshots]
+    --window-size, -w <type> Browser window size [Default: 1300,1300]
+    --chrome-path, -p <type> Path to Chrome [Default: /Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome]
 
-	Examples
-	  $ website-screenshot --name=Jane
-	  Hello, Jane
+  Examples
+    $ your-command --url "http://example.com"
 `,
-	{
-		importMeta: import.meta,
-		flags: {
-			name: {
-				type: 'string',
-			},
-		},
-	},
-);
+  {
+    importMeta: import.meta,
+    flags: {
+      url: { type: 'string', shortFlag: 'u', isRequired: true },
+      transform: { type: 'boolean', shortFlag: 't', default: false },
+      timeout: { type: 'number', shortFlag: 'T', default: 180000 },
+      extension: { type: 'string', shortFlag: 'e', default: 'webp' },
+      concurrency: { type: 'number', shortFlag: 'c', default: 3 },
+      quality: { type: 'number', shortFlag: 'q', default: 100 },
+      maxScreenshots: { type: 'number', shortFlag: 'm', default: 0 },
+      recommendedSize: {
+        type: 'string',
+        shortFlag: 'r',
+        default: JSON.stringify({ width: 1200, height: 630 }),
+        parse: parseJson,
+      },
+      outputDir: { type: 'string', shortFlag: 'o', default: './public/screenshots' },
+      windowSize: { type: 'string', shortFlag: 'w', default: '1300,1300' },
+      chromePath: {
+        type: 'string',
+        shortFlag: 'p',
+        default: `/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome`,
+      },
+    },
+  }
+)
 
-render(<App name={cli.flags.name} />);
+render(<App name={cli.flags.url} />)
